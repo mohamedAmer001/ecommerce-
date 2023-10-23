23ebac1913e79021fdcc669824250fddfd6442ca
Mustafa Ahmed <134523270+mustafahmed1@users.noreply.github.com>
2023-10-23 15:20:38 +0300

    Add files via upload
    https://roaring-beijinho-6c7c31.netlify.app

---
+++ wesite 1/script.js
@@ -0,0 +1,29 @@
+const bar = document.getElementById('bar')
+const navbar = document.getElementById('navbar')
+const close = document.getElementById('close')
+if (bar) {
+    bar.addEventListener('click',()=> {
+        navbar.classList.add('active')
+    })
+}
+if (close){
+    close.addEventListener('click',()=> {
+        navbar.classList.remove('active')
+    })
+}
+
+var MainImg =document.getElementById('main-img')
+var SmallImg =document.getElementsByClassName('small-img')
+SmallImg[0].onclick = function(){
+    MainImg.src = SmallImg[0].src
+}
+SmallImg[1].onclick = function(){
+    MainImg.src = SmallImg[1].src
+}
+SmallImg[2].onclick = function(){
+    MainImg.src = SmallImg[2].src
+}
+SmallImg[3].onclick = function(){
+    MainImg.src = SmallImg[3].src
+}
+
